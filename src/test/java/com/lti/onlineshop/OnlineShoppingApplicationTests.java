eba20447d4b536fb6d8758b22d65bf19318e83b1
shubhamshinde4141 <shubham.21810417@viit.ac.in>
2022-07-28 09:58:21 +0530

    First Commit

---
+++ src/test/java/com/lti/onlineshop/OnlineShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
